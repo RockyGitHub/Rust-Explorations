651053d2349b20a6dd8a022c4259affc3a3c8a95
Rocky_Felix <chris.davidoff>
2022-09-30 14:54:09 -0600

    asd

--- sled-stuff/src/main.rs
+++ sled-stuff/src/main.rs
@@ -1,10 +1,32 @@
+use std::{thread, time::Duration};
+use chrono::Utc;
+
 fn main() {
     println!("Hello, world!");
 
+    let config = sled::Config::new().temporary(true);
+    let db = config.open().unwrap();
 
-    let db = sled::open("mydb").unwrap();
+    db.insert(Utc::now().to_string(), "hi").unwrap();
+    thread::sleep(Duration::from_millis(200));
+    db.insert(Utc::now().to_string(), "hello").unwrap();
+    thread::sleep(Duration::from_millis(200));
+    db.insert(Utc::now().to_string(), "sup").unwrap();
+    thread::sleep(Duration::from_millis(200));
+    db.insert(Utc::now().to_string(), "asdf").unwrap();
+    thread::sleep(Duration::from_millis(200));
+    db.insert(Utc::now().to_string(), "howdy").unwrap();
 
-    db.insert(&[1], &[2]).unwrap();
-    
+    for thing in db.iter() {
+        println!(
+            "{}",
+            String::from_utf8(thing.unwrap().1.as_ref().to_vec()).unwrap()
+        );
+    }
+
+    for thing in db.iter().flatten() {
+        println!("{}", String::from_utf8(thing.1.as_ref().to_vec()).unwrap());
+    }
+
     drop(db);
 }
